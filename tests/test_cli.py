af224227f7300fa2d13b78157a22dcd729157d37
Vitor Loureiro <miseravel.cruller-0o@icloud.com>
2022-02-16 03:03:06 -0300

    bumped to version 0.1.1

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,7 @@ from properjpg.cli import run
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
 
 
 @pytest.mark.parametrize("output_path", [None, "custom_output.jpg"])
